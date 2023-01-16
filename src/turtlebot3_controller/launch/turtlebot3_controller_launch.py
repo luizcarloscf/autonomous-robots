a4a06097076d441379a00ec5cff1236404bef02f
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-01-16 17:46:44 -0300

    :page_facing_up: Add copyright statement to all files (#5)
    * Update to Apache license
    * Add copyright statement in package turtlebot3_controller
    * Fix copyright comments
    * Add copyright statement in package turtlesim_controller
    * Add contributing guide
    
    Signed-off-by: Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>

--- src/turtlebot3_controller/launch/turtlebot3_controller_launch.py
+++ src/turtlebot3_controller/launch/turtlebot3_controller_launch.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Luiz Carlos Cosmi Filho and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 from launch_ros.actions import Node
@@ -38,6 +52,9 @@ def generate_launch_description():
             },
             {
                 "tolerance": 0.1
+            },
+            {
+                "rate": 20.0
             },
         ],
     )
